f79141330a86098fa3f2b82339b2bf25b7745c61
arizafilms <34016493+arizafilms@users.noreply.github.com>
2017-12-01 14:38:34 -0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -3,43 +3,21 @@ var $wHeight = $(window).height();
 var $wWidth = $(window).width();
 var $links = $('.link');
 var $menuBtn = $('.menu-icon');
-var $wScroll=$(window).scrollTop();
-var $leftPageTop = $('.left-title-page').offset().top;
-var $rightPageTop =  $('.right-content').offset().top;
-
-console.log($rightPageTop);
-
-$(window).scroll(function(){
-    var $wScroll=$(window).scrollTop();
+var $wScroll = $(window).scrollTop();
 
 
 
-
-    if($wScroll > $leftPageTop *.3){
-        $('.page-name').addClass('is-showing');
-
-    }else{
-        $('.page-name').removeClass('is-showing');
-
-    }
-
-
-    if($wScroll > $rightPageTop*.5){
-        $('.about-me').fadeIn(900);
-    }else{
-        $('.about-me').fadeOut(500);
-    }
-
-
+$(window).scroll(function() {
+    var $wScroll = $(window).scrollTop();
 });
 
-$menuBtn.click(function(){
+$menuBtn.click(function() {
     var $wWidth = $(window).width();
-    if($wWidth >500 && $wWidth <820){
+    if ($wWidth > 500 && $wWidth < 820) {
         $('.links-container').animate({
             'width': 'toggle'
         });
-    }else {
+    } else {
         $('.links-container').slideToggle(300);
     }
 
@@ -58,10 +36,10 @@ $links.click(function() {
     var href = $.attr(this, 'href');
     $root.animate({
         scrollTop: $(href).offset().top
-    },500, function () {
+    }, 500, function() {
         window.location.hash = href;
     });
-    $('#audio-player-project').click(function () {
+    $('#audio-player-project').click(function() {
         this.href = $(this).attr('data-url');
         console.log($(this).attr('data-url'));
         window.open(this.href);
