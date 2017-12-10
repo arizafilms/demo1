674231e0411cb6a6715993bad284e3047fac5c9b
arizafilms <34016493+arizafilms@users.noreply.github.com>
2017-12-09 19:05:24 -0500

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -5,45 +5,20 @@ var $links = $('.link');
 var $menuBtn = $('.menu-icon');
 var $wScroll = $(window).scrollTop();
 
-
-
-$(window).scroll(function() {
+/*$(window).scroll(function() {
     var $wScroll = $(window).scrollTop();
-});
-
-/*$menuBtn.click(function() {
-    var $wWidth = $(window).width();
-    if ($wWidth > 500 && $wWidth < 820) {
-        $('.links-container').animate({
-            'width': 'toggle'
-        });
-    } else {
-        $('.links-container').slideToggle(300);
-    }
-
 });*/
+
 $links.click(function() {
     var $wWidth = $(window).width();
-
-    /* if($wWidth >500 && $wWidth <820){
-        $('.links-container').animate({
-            'width': 'toggle'
-        });
-    }else {
-        $('.links-container').slideToggle(300);
-    }*/
 
     var href = $.attr(this, 'href');
     $root.animate({
         scrollTop: $(href).offset().top
+
+
     }, 500, function() {
+        console.log($(href).offset().top);
         window.location.hash = href;
     });
-    $('#audio-player-project').click(function() {
-        this.href = $(this).attr('data-url');
-        console.log($(this).attr('data-url'));
-        window.open(this.href);
-        return false;
-    })
-
 });
