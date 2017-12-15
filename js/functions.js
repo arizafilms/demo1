efb092dc2ae79ecc727453d36ab8be24c40584e0
arizafilms <34016493+arizafilms@users.noreply.github.com>
2017-12-15 13:19:10 -0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -5,19 +5,55 @@ var $links = $('.link');
 var $menuBtn = $('.menu-icon');
 var $wScroll = $(window).scrollTop();
 
-/*$(window).scroll(function() {
-    var $wScroll = $(window).scrollTop();
-});*/
+
+setTimeout(function() {
+
+    $root.on('wheel', function(e) {
+        var $wHeight = $(window).height();
+
+
+        var $wScroll = $(window).scrollTop();
+        var scrollDown = $wScroll + $wHeight;
+        var scrollUp = $wScroll - $wHeight;
+
+
+        if (e.originalEvent.wheelDelta < 0) {
+            //scroll down
+            setTimeout(function() {
+
+
+                console.log('Down');
+                $root.animate({
+                    scrollTop: scrollDown
+                }, 500);
+            }, 200);
+
+
+        } else {
+            //scroll up
+            setTimeout(function() {
+
+                console.log('Up');
+                $root.animate({
+                    scrollTop: scrollUp
+                }, 200);
+            }, 200);
+
+        }
+
+        //prevent page fom scrolling
+        return false;
+    });
+}, 100);
+
 
 $links.click(function() {
-    var $wWidth = $(window).width();
-
     var href = $.attr(this, 'href');
     $root.animate({
         scrollTop: $(href).offset().top
 
 
-    }, 500, function() {
+    }, 200, function() {
         console.log($(href).offset().top);
         window.location.hash = href;
     });
