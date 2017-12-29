a046268cacf094f87e6f617f5eadd06ca9975e02
arizafilms <34016493+arizafilms@users.noreply.github.com>
2017-12-28 23:32:11 -0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -103,7 +103,7 @@ function snap($wScroll, e) {
         case $wScroll < (($pages[1].offsetTop * 4.45)):
             $('.link:eq(2)').trigger('click');
             tv.unMute();
-            tv.playVideo();
+            tv.play();
             console.log('video');
             break;
         case $wScroll < (($pages[1].offsetTop * 5.35)):
