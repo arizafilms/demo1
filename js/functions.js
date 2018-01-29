3722d57a94070a9b2bdb5989783c665e3e48050c
arizafilms <34016493+arizafilms@users.noreply.github.com>
2018-01-29 16:32:17 -0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -11,13 +11,15 @@ var $pages = $('.page');
 var timer = null;
 var $logo = $('.logo');
 var didScroll = false;
+var scrollSpeed = 650;
+
 
 
 function fadeMenu() {
     var $wScroll = $(window).scrollTop();
     var $playerTop = $('#video').offset().top;
     var $playerBottom = $('#me').offset().top;
-    
+
     if ($wScroll >= ($playerTop - 100) && $wScroll < ($playerBottom - 300)) {
         $navBar.css({
             'opacity': '.5'
@@ -31,71 +33,88 @@ function fadeMenu() {
 
 
 function snap() {
-    
+
     $wScroll = $(window).scrollTop();
     didScroll = true;
     switch (didScroll) {
-        case $wScroll < ($pages[1].offsetTop * 0.5):
+        case $wScroll < ($pages[1].offsetTop * 0.2):
             $root.animate({
                 scrollTop: $pages[1].offsetTop * 0
-            }, 800);
-            
+            }, scrollSpeed);
+            tv.setVolume(0);
+            didScroll = false;
             break;
-        case $wScroll < ($pages[1].offsetTop * 1.45):
+        case $wScroll < ($pages[1].offsetTop * 1.4):
             $root.animate({
                 scrollTop: $pages[1].offsetTop * 1
-            }, 800);
+            }, scrollSpeed);
             didScroll = false;
+            tv.setVolume(0);
+
             break;
-        case $wScroll < (($pages[1].offsetTop * 2.45)):
+        case $wScroll < (($pages[1].offsetTop * 2.4)):
             $root.animate({
                 scrollTop: $pages[1].offsetTop * 2
-            }, 800);
+            }, scrollSpeed);
             didScroll = false;
+            tv.setVolume(0);
+
             break;
-        case $wScroll < (($pages[1].offsetTop * 3.45)):
+        case $wScroll < (($pages[1].offsetTop * 3.4)):
             $root.animate({
                 scrollTop: $pages[1].offsetTop * 3
-            }, 800);
+            }, scrollSpeed);
             didScroll = false;
+            tv.setVolume(0);
+
             break;
-        case $wScroll < (($pages[1].offsetTop * 4.45)):
+        case $wScroll < (($pages[1].offsetTop * 4.4)):
             $root.animate({
                 scrollTop: $pages[1].offsetTop * 4
-            }, 800);
+            }, scrollSpeed);
             didScroll = false;
+            tv.setVolume(0);
+
             break;
-        case $wScroll < (($pages[1].offsetTop * 5.45)):
+        case $wScroll < (($pages[1].offsetTop * 5.4)):
             $root.animate({
                 scrollTop: $pages[1].offsetTop * 5
-            }, 800);
+            }, scrollSpeed);
+            tv.playVideo();
+            tv.setVolume(30);
             var didScroll = false;
             break;
         case $wScroll < (($pages[1].offsetTop * 6.35)):
             $root.animate({
                 scrollTop: $pages[1].offsetTop * 6
-            }, 800);
+            }, scrollSpeed);
             didScroll = false;
+            tv.setVolume(0);
+
             break;
     }
 }
 $(window).on('scroll', function() {
-    
+    $wHeight = $(window).height();
     didScroll = true;
     $playerTop = $('#video').offset().top;
     $playerBottom = $('#me').offset().top;
     $wScroll = $(window).scrollTop();
 
+    var fadeSpeed = (1 - ($wScroll / $wHeight) * 1.5);
+    console.log(fadeSpeed);
     fadeMenu(); //fades the menu bar/icons when 
     //video player is in view
 
     if (timer !== null) {
         clearTimeout(timer);
     }
-    timer = this.setTimeout(snap, 900);
+    timer = this.setTimeout(snap, 950);
 
+    //pull logo down on scroll from home page
     $logo.css({
-        'transform': 'translate( ' + 0 + '%, ' + ($wScroll * 0.19) + '%)'
+        'transform': 'translateY(-' + ($wScroll * 0.15) + '%)',
+        'opacity': fadeSpeed
     });
 
     if ($wScroll > $('#portfolio').offset().top * 0.05) {
@@ -103,20 +122,20 @@ $(window).on('scroll', function() {
             setTimeout(function() {
                 $('#lightgallery img').eq(i).animate({
                     'opacity': '1'
-                }, 1000);
-            }, 950 * (i + 1));
+                }, 500);
+            }, 250 * (i + 1));
         });
     }
 });
 
 
 $links.on('click', function() {
-    
+
     href = $.attr(this, 'href');
 
     $root.animate({
         scrollTop: $(href).offset().top
-    }, 1000);
+    }, scrollSpeed);
 
     window.location.hash = href;
 });
